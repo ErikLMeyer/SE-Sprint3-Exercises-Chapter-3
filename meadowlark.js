b12368d963b9727ca95eb9c7f17a2675e9477045
ErikLMeyer <erikmeyer@lewisu.edu>
2021-02-24 17:33:08 -0600

    Initial commit

---
+++ meadowlark.js
@@ -0,0 +1,51 @@
+/*
+    Code copied from Web Development with Express and Node, 2nd Edition
+    by Ethan Brown
+
+    Semicolons added by me, Erik L. Meyer
+*/
+
+const express = require('express');
+const expressHandelbars = require('express-handlebars');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const fortunes = [
+    "Conquer fears",
+    "Rivers need springs",
+    "Don't fear unknown",
+    "Pleasant surprise!",
+    "Keep it simple"
+]
+
+app.use(express.static(__dirname + '/public'));
+
+// Configure Handlebars view engine
+app.engine('handlebars', expressHandelbars({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => res.render('home'));
+
+app.get('/about', (req, res) => {
+    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
+    res.render('about', {fortune: randomFortune});
+})
+
+// Custom 404 page
+app.use((req, res) => {
+    res.status(404);
+    res.render('404');
+})
+
+// Custom 500 page
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.status(500);
+    res.render('500');
+})
+
+app.listen(port, () => console.log(
+    `Express started on http://localhost:${port}; ` +
+    `press Ctrl-C to terminate.`))
